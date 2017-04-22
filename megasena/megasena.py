1d2c681214979a5b18fdc306948b428a63a14a42
Felipe Reis <felipe.s.reis@live.com>
2017-04-22 17:05:22 -0300

    Ajustes

--- megasena/megasena.py
+++ megasena/megasena.py
@@ -1,13 +1,13 @@
-#coding: utf-8
+# coding: utf-8
 import random
 
-#meu jogo
-meu_jogo = [20, 50, 30, 22, 10, 13]
+# meu jogo
+meu_jogo = [20, 11, 30, 22, 10, 13]
 
-#variavel para armazenar todos os numeros da megasena através do método construtor list
+# variavel para armazenar todos os numeros da megasena através do método construtor list
 megasena = list(range(1, 61))
 
-#variavel para saber o numero de testes a realizar
+# variavel para saber o numero de testes a realizar
 n_teste = int(input("Digite o numero de testes: "))
 soma = 0
 
@@ -20,23 +20,19 @@ for i in range(n_teste):
         mega_sorteio = megasena.copy()
         sorteado = []
 
-        for j in range(6): #numero de sorteios
+        for j in range(6):  # numero de sorteios
             num_sorteado = random.choice(mega_sorteio)
             sorteado.append(num_sorteado)
             mega_sorteio.remove(num_sorteado)
 
-        #organizar
+        # organizar
         sorteado.sort()
         ct += 1
 
-    #variavel para calcular a média
+    # variavel para calcular a média
     soma += ct
 
-    print("O resultado do teste %d: %d tentativas" % (i+1, ct))
+    print("O resultado do teste %d: %d tentativas" % (i + 1, ct))
 
 soma /= n_teste
 print("Média dos resultados:", soma)
-
-"""
-Observação:
-"""
